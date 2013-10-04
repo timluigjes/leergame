39784d4d5d0bfb5b39987eb2f1b9f52b6de3decb
pictor <tim@pictor.ws>
2013-10-04 17:02:46 +0200

    Update config.php

--- include/config.php
+++ include/config.php
@@ -5,9 +5,9 @@ session_start();
 //ini_set('display_errors', 'on'); 
 
 $dbhost = "localhost"; // this will ususally be 'localhost', but can sometimes differ  
-$dbname = "deb14232n4_lg"; // the name of the database that you are going to use for this project  
-$dbuser = "deb14232n4_lgad"; // the username that you created, or were given, to access your database  
-$dbpass = "nH2N8atq"; // the password that you created, or were given, to access your database 
+$dbname = ""; // the name of the database that you are going to use for this project  
+$dbuser = ""; // the username that you created, or were given, to access your database  
+$dbpass = ""; // the password that you created, or were given, to access your database 
 
 $con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname) or die("MySQL Error: " . mysqli_error($con)); 
 
